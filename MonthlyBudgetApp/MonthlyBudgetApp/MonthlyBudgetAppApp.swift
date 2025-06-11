81e8b33df210ace51f5b4e21ebb306ef562a6552
raizakurniawan <raizakurniawan@gmail.com>
2025-06-11 13:06:06 +0700

    Initial project setup with Xcode

---
+++ MonthlyBudgetApp/MonthlyBudgetApp/MonthlyBudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyBudgetAppApp.swift
+//  MonthlyBudgetApp
+//
+//  Created by raizakurniawan on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyBudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonthlyBudgetApp/MonthlyBudgetAppTests/MonthlyBudgetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyBudgetAppTests.swift
+//  MonthlyBudgetAppTests
+//
+//  Created by raizakurniawan on 11/06/25.
+//
+
+import Testing
+@testable import MonthlyBudgetApp
+
+struct MonthlyBudgetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
